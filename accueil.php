925354ca8a204c91ecbc42202ac81a1b5116de2a
tdassise <77011334+tdassise@users.noreply.github.com>
2022-02-15 23:27:45 +0100

    Update accueil.php

--- accueil.php
+++ accueil.php
@@ -1,12 +1,30 @@
-<?php ?>
+<?php
+$navigateur = $_SERVER['HTTP_USER_AGENT']; // avoir toutes les infos avec get_browser (méthode php)
+?>
 <!DOCTYPE html>
 <html lang="">
-    <head>
-        <title>Accueil</title>
-    </head>
-    <body>
-        <h1>
-            Hello World
-        </h1>
-    </body>
+<head>
+    <title>Connexion</title>
+    <link rel="stylesheet" href="../css/base.css">
+    <link rel="stylesheet" href="../css/accueil.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Sora:wght@200&display=swap" rel="stylesheet">
+</head>
+<header class="police">
+</header>
+<body>
+<div class="div-center police">
+    <div class="accueil-titre texte">Accueil</div>
+    <div class="texte">
+        <div class="cadre-accueil">
+            Navigateur web : <?php echo $navigateur ?>
+        </div>
+        <div style="z-index: -1">wizzhezio</div>
+    </div>
+</div>
+<img class="taille-ecran" src="../images/nts_vue_du_ciel.jpeg" alt="Nantes vue du ciel">
+</body>
+<footer>
+</footer>
 </html>
