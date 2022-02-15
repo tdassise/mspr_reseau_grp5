38ac5abbfe4cd93bf4e8a031fee1cd3d0b98d273
julien belinga <58934209+julienBelinga@users.noreply.github.com>
2022-02-15 14:59:08 +0100

    create

---
+++ login.php
@@ -0,0 +1,23 @@
+<?php ?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>mspr_reseau_grp5</title>
+        <script>
+
+        </script>
+    </head>
+    <body>
+        <h1>Login</h1>
+        <div style="display: grid; width: 500px; border: solid 1px #000; padding: 10px;">
+            <label for="">pseudo</label>
+            <input type="text">
+            <label>password</label>
+            <input type="password">
+            <button onclick="" style="margin-top: 10px;">Login</button>
+        </div>
+    </body>
+    <footer>
+
+    </footer>
+</html>
