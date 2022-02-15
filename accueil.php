cb729aa35650d19196e94f608b09bf0928f88680
tdassise <77011334+tdassise@users.noreply.github.com>
2022-02-15 15:41:49 +0100

    ajout Accueil.php
    oui

---
+++ accueil.php
@@ -0,0 +1,12 @@
+<?php ?>
+<!DOCTYPE html>
+<html lang="">
+    <head>
+        <title>Accueil</title>
+    </head>
+    <body>
+        <h1>
+            Hello World
+        </h1>
+    </body>
+</html>
