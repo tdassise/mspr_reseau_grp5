fa011b9cb7463c7b2d212fccfcc59208ad91ec28
julien belinga <58934209+julienBelinga@users.noreply.github.com>
2022-03-04 01:36:47 +0100

    update BDD with authentication
    password hashed in the datebase

--- mspr_grp5.sql
+++ mspr_grp5.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.0
+-- version 5.0.2
 -- https://www.phpmyadmin.net/
 --
--- Hôte : localhost:8889
--- Généré le : jeu. 17 fév. 2022 à 14:22
--- Version du serveur :  5.7.34
--- Version de PHP : 8.0.8
+-- Hôte : 127.0.0.1:3306
+-- Généré le : ven. 04 mars 2022 à 00:35
+-- Version du serveur :  5.7.31
+-- Version de PHP : 7.3.21
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,15 +24,41 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Structure de la table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `createdAt` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `users`
+--
+
+INSERT INTO `users` (`id`, `email`, `password`, `createdAt`) VALUES
+(1, 'ju@example.com', '1234', '2022-03-04 00:41:18'),
+(2, 'jean@exemple.fr', '$2y$10$0D8U7K/lcX9JXicntLhJc.9.sbdOMZwuyabnIHUho3ZYLlX5UjlSe', '2022-03-04 01:22:33'),
+(3, 'jean@exemple.fr', '$2y$10$0k1ALs1v8DTknrqouhHBh.8vW7KHVU1sFTbZ0OSlGUmELV4zMmSwi', '2022-03-04 01:22:40');
+
+-- --------------------------------------------------------
+
+--
 -- Structure de la table `verification`
 --
 
-CREATE TABLE `verification` (
-  `id` int(11) NOT NULL,
+DROP TABLE IF EXISTS `verification`;
+CREATE TABLE IF NOT EXISTS `verification` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(20) NOT NULL,
   `navigateur` varchar(200) NOT NULL,
-  `adresse_ip` varchar(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `adresse_ip` varchar(20) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
 
 --
 -- Déchargement des données de la table `verification`
@@ -43,26 +69,6 @@ INSERT INTO `verification` (`id`, `username`, `navigateur`, `adresse_ip`) VALUES
 (2, 'thibault', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.3 Safari/605.1.15', '127.0.0.1'),
 (3, 'thibault', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.3 Safari/605.1.15', '127.0.0.1'),
 (4, 'thibault', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.3 Safari/605.1.15', '127.0.0.1');
-
---
--- Index pour les tables déchargées
---
-
---
--- Index pour la table `verification`
---
-ALTER TABLE `verification`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT pour les tables déchargées
---
-
---
--- AUTO_INCREMENT pour la table `verification`
---
-ALTER TABLE `verification`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
