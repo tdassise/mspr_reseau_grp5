f30cadc9465245cce55dcb16d1fa8a22e24e4656
julien belinga <58934209+julienBelinga@users.noreply.github.com>
2022-03-05 16:01:49 +0100

    create design login page

--- pages/index.php
+++ pages/index.php
@@ -2,6 +2,8 @@
 session_start();
 
 require('../database/config_db.php');
+session_destroy();
+require('../database/db.php');
 require '../database/Requetes.php';
 
 $navigateur = $_SERVER['HTTP_USER_AGENT'];
@@ -9,6 +11,26 @@ $adresse_ip = $_SERVER['REMOTE_ADDR'];
 
 $req = new Requetes();
 $req->insert($navigateur, $adresse_ip);
+
+//register form
+if(!empty($_POST['email']) && !empty($_POST['password'])){
+    $email = $_POST['email'];
+    $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+
+    if($debug == 1){
+        var_dump($email);
+        var_dump($password);
+    }
+
+    $q = $db->prepare('INSERT INTO users (email, password) VALUES (:email, :password)');
+    $q->bindValue('email', $email);
+    $q->bindValue('password', $password);
+    $res = $q->execute();
+
+    if($res){
+        echo 'register succed';
+    }
+}
 
 ?>
 
@@ -26,30 +48,45 @@ $req->insert($navigateur, $adresse_ip);
 </header>
 <body>
 
-    <div class="div-center police">
-        <div class="login-titre texte">Clinique le Châtelet</div>
-        <div class="pad-left texte">
-
-        <!-- solution with database, waiting for LDAP -->
-        <form method="POST" action="register.php">
-            <input type="email" placeholder="Email" name="email"></br>
-            <input type="password" placeholder="password" name="password"></br>
-            <button type="submit">Register</button>
-        </form>
-            
-        <hr>
-
-        <form method="POST" action="login.php">
-                <input type="email" placeholder="email" name="email"></br>
-                <input type="password" placeholder="password" name="password"></br>
-                <input type="text" placeholder="verification code" name="tfa_code"></br>
-                <button type="submit">Login</button>
-        </form>
-
+    <div id="main-container">
+        <div id="form-box">
+            <div class="button-box">
+                <div id="btn"></div>
+                <button type="button" class="toggle-btn" onclick="login()">Login</button>
+                <button type="button" class="toggle-btn" onclick="register()">Register</button>
+            </div>
+            <form id="login" class="input-group" method="POST" action="login.php">
+                <input class="input-field" type="email" placeholder="Email" name="email"></br>
+                <input class="input-field" type="password" placeholder="Password" name="password"></br>
+                <input class="input-field" type="text" placeholder="verification code" name="tfa_code"></br>
+                <button class="submit-btn" type="submit">Login</button>
+            </form>
+            <form id="register" class="input-group" method="POST">
+                <input class="input-field" type="email" placeholder="Email" name="email"></br>
+                <input class="input-field" type="password" placeholder="Password" name="password"></br>
+                <button class="submit-btn" type="submit">Register</button></br>
+            </form>
         </div>
     </div>
 
-    <img class="taille-ecran" src="../images/nts_vue_du_ciel.jpeg" alt="Nantes vue du ciel">
+    <script>
+        var x = document.getElementById("login");
+        var y = document.getElementById("register");
+        var z = document.getElementById("btn");
+
+        //form animation
+        function register(){
+            x.style.left = "-400px";
+            y.style.left = "50px";
+            z.style.left = "110px";
+        }
+
+        function login(){
+            x.style.left = "50px";
+            y.style.left = "450px";
+            z.style.left = "0";
+        }
+    </script>
     
 </body>
 <footer>
