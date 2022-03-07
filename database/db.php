6b536a1939c7a827a6ab67bb7ae61f71b42945e7
julien belinga <58934209+julienBelinga@users.noreply.github.com>
2022-03-07 02:31:31 +0100

    started anti brute-force mechanism

--- database/db.php
+++ database/db.php
@@ -10,3 +10,14 @@ try{
 }catch(PDOException $e){
     echo 'connexion failed :'.$e->getMessage();
 }
+
+function loginAttempts($loginAttempts, $id, $db){
+    $loginAttempts += 1;
+    $q = $db->prepare('UPDATE users SET loginAttempt = '.$loginAttempts.' WHERE id = :'.$id.'');
+    $q->execute();
+}
+function clearLoginAttempts($id, $db){
+    $loginAttempts = 0;
+    $q = $db->prepare('UPDATE users SET loginAttempt = '.$loginAttempts.' WHERE id = :'.$id.'');
+    $q->execute();
+}
